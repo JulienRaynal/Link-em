1256865d2ca9f320b0b7d67103f0554411bc1e85
KyabeSamaEPSI <59728960+KyabeSamaEPSI@users.noreply.github.com>
2020-06-14 22:47:25 +0200

    Add files via upload

---
+++ lambda/localisation.js
@@ -0,0 +1,17 @@
+module.exports = {
+    fr: {
+        translation: {
+            WELCOME_MSG: 'Bienvenue sur la Skill linkEm! ',
+            WELCOME_BACK_MSG: 'Content de vous revoir ! ',
+            REJECTED_MSG: "Une erreur est survenue. Veuillez réessayer",
+            MISSING_MSG: 'Il me semble que vous n\'avez pas mentionné d\'appareil ',
+            POST_SAY_HELP_MSG: `BLOUP`,
+            HELP_MSG: 'Je suis là pour vous aider à vous occuper de vos appareils connectés',
+            REPROMPT_MSG: `Pour obtenir plus d'informations sur ce que je peux faire pour vous, demandez-moi de l'aide. Si vous voulez quitter la Skill, dites simplement "stop". Quel est votre choix ?`,
+            GOODBYE_MSG: 'Au revoir! ',
+            REFLECTOR_MSG: 'Vous avez invoqué l\'intention {{intent}}',
+            FALLBACK_MSG: 'Désolé, je ne sais pas répondre à votre demande. Pouvez-vous reformuler?. ',
+            ERROR_MSG: 'Désolé, je n\'ai pas compris. Pouvez-vous reformuler? '
+        }
+    }
+}
